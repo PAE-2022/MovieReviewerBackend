b9f611af866905f83db89a368c0b5b3b3ca88fe3
Alan Ramírez Herrera <alan5142@hotmail.com>
2022-04-12 19:58:25 -0500

    feat: First commit, project set up

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin', 'unused-imports'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jasmine: true
+  },
+  ignorePatterns: [
+    '.eslintrc.js', 
+    "node_modules", 
+    "build",
+    "dist",
+    "coverage",
+    '**/*jasmine.js'
+  ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 2,
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        'selector': 'interface',
+        'format': ['PascalCase'],
+        'custom': {
+          'regex': '^I[A-Z]',
+          'match': true
+        }
+      }
+    ]
+  },
+};
